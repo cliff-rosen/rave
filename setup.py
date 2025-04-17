4a729c4be962ec89e94a7536a695adcad55ed6fd
Cliff Rosen <cliff.rosen@gmail.com>
2025-04-17 11:45:55 -0600

    backup

--- setup.py
+++ setup.py
@@ -9,14 +9,13 @@ setup(
         "langchain==0.1.12",
         "langchain-openai==0.0.8",
         "langgraph==0.0.27",
-        "pydantic==2.6.4",
-        "pydantic-core==2.16.2",
+        "pydantic==1.10.13",
+        "pydantic-core==1.10.13",
         "tavily-python==0.3.1",
         "typing-extensions==4.10.0",
         "openai==1.12.0",
-        "langsmith==0.1.0",
-        "python-dotenv",
+        "python-dotenv==1.0.1"
     ],
     package_dir={"": "."},
-    python_requires=">=3.8",
+    python_requires=">=3.8,<3.12",  # Restrict to Python versions before 3.12
 ) 
