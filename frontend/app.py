50e2309af8427d83ebb9b3588541a1475689ff10
Cliff Rosen <cliff.rosen@gmail.com>
2025-04-24 12:12:59 -0600

    backup

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import time
-from backend.agents.rave_agent import search2, get_best_urls_from_search, scrape_urls
+from backend.agents.rave_agent import search, search2, get_best_urls_from_search, scrape_urls
 
 state = {
     "question": "i'd like to better understand how the economy works",
@@ -21,7 +21,23 @@ if st.button("reset"):
     st.session_state.cur_idx = 0
 
 
-def search():
+def search_wrapper():
+    st.session_state.search_status = "searching..."
+    st.session_state.search_results = []
+    st.session_state.urls_to_scrape = []
+    st.session_state.scraped_content = ["-"]
+
+    st.session_state.search_status_container.write("searching...")
+    st.session_state.state["current_query"] = st.session_state.state["current_query"]
+    print("state", st.session_state.state)
+    res = search(
+            st.session_state.state,
+            writer=None
+        )
+    st.session_state.state_container.write("search completed")
+    st.session_state.state["search_results"] = res["search_results"]
+
+def search2_wrapper():
     st.session_state.search_status = "searching..."
     st.session_state.search_results = []
     st.session_state.urls_to_scrape = []
@@ -37,6 +53,7 @@ def search():
         )
     st.session_state.state_container.write("search completed")
     st.session_state.state["search_results"] = res["search_results"]
+
 
 def get_best_urls():
     st.session_state.search_status_container.write("getting best urls...")
@@ -67,7 +84,12 @@ def prev_url():
     st.session_state.cur_idx -= 1
 
 with left_col:
-    st.button("search2", on_click=search)
+    st.button("search", on_click=search_wrapper)
+    st.write("STATUS")
+    st.session_state.search_status_container = st.empty()
+
+with left_col:
+    st.button("search2", on_click=search2_wrapper)
     st.write("STATUS")
     st.session_state.search_status_container = st.empty()
 
