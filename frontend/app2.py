658ffb0bab31332ef04971c0b1690c118682bac5
Cliff Rosen <cliff.rosen@gmail.com>
2025-04-24 12:22:24 -0600

    backup

--- frontend/app2.py
+++ frontend/app2.py
@@ -6,6 +6,8 @@ import os
 # Add the project root directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from backend.agents.rave_agent import search, search2, get_best_urls_from_search, scrape_urls
+
 state = {
     "question": "i'd like to better understand how the economy works",
     "improved_question": "i'd like to better understand how the economy works",
@@ -25,22 +27,38 @@ if st.button("reset"):
     st.session_state.cur_idx = 0
 
 
-def search():
+def search_wrapper():
     st.session_state.search_status = "searching..."
     st.session_state.search_results = []
     st.session_state.urls_to_scrape = []
     st.session_state.scraped_content = ["-"]
 
     st.session_state.search_status_container.write("searching...")
-    st.session_state.state["current_query"] = st.session_state.query
+    st.session_state.state["current_query"] = st.session_state.state["current_query"]
     print("state", st.session_state.state)
-    res = search2(
+    res = search(
             st.session_state.state,
-            writer=None,
-            config={"configurable": {"max_search_results": 4}}
+            writer=None
         )
     st.session_state.state_container.write("search completed")
     st.session_state.state["search_results"] = res["search_results"]
+
+def search2_wrapper():
+    st.session_state.search_status = "searching..."
+    st.session_state.search_results = []
+    st.session_state.urls_to_scrape = []
+    st.session_state.scraped_content = ["-"]
+
+    st.session_state.search_status_container.write("searching...")
+    st.session_state.state["current_query"] = st.session_state.state["current_query"]
+    print("state", st.session_state.state)
+    res = search2(
+            st.session_state.state,
+            writer=None
+        )
+    st.session_state.state_container.write("search completed")
+    st.session_state.state["search_results"] = res["search_results"]
+
 
 def get_best_urls():
     st.session_state.search_status_container.write("getting best urls...")
@@ -71,16 +89,14 @@ def prev_url():
     st.session_state.cur_idx -= 1
 
 with left_col:
-    st.text_input("question", on_change=search, key="query")
-    st.button("get best urls", on_click=get_best_urls)
-    st.button("scrape urls", on_click=scrape_urls_from_best_urls)  
+    st.button("search", on_click=search_wrapper)
     st.write("STATUS")
     st.session_state.search_status_container = st.empty()
-    prev_col, next_col = st.columns([1,1])    
-    with prev_col:
-        st.button("prev", on_click=prev_url)
-    with next_col:
-        st.button("next", on_click=next_url)
+
+with left_col:
+    st.button("search2", on_click=search2_wrapper)
+    st.write("STATUS")
+    st.session_state.search_status_container = st.empty()
 
 
 with right_col:
@@ -88,14 +104,7 @@ with right_col:
     st.session_state.state_container = st.empty()
     st.session_state.state_container.write(st.session_state.state)
 
-    st.session_state.scraped_content_container = st.empty()
-    container = st.container()
-    if st.session_state.state["scraped_content"]:
-        with container:
-            st.write(f"idx: {st.session_state.cur_idx}")
-            st.write(f"url: {st.session_state.state['urls_to_scrape'][st.session_state.cur_idx]}")
-            st.markdown(st.session_state.state["scraped_content"][st.session_state.cur_idx])
-
 st.markdown("---")
 st.write("DEBUG")
+st.write("v.0.2")
 
